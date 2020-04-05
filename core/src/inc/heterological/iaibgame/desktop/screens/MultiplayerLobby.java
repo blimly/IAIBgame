9b797b14d9b6298183f23e1ffab115c7a637849f
Maario Lemmik <malemm@ttu.ee>
2020-04-05 11:36:06 +0300

    MultiplayerLobby -> extends GameState This state overwritten with GameStateManager implementation

--- core/src/inc/heterological/iaibgame/desktop/screens/MultiplayerLobby.java
+++ core/src/inc/heterological/iaibgame/desktop/screens/MultiplayerLobby.java
@@ -1,69 +1,65 @@
 package inc.heterological.iaibgame.desktop.screens;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import inc.heterological.iaibgame.Main;
 import inc.heterological.iaibgame.desktop.Assets;
+import inc.heterological.iaibgame.desktop.managers.GameKeys;
+import inc.heterological.iaibgame.desktop.managers.GameStateManager;
 
-public class MultiplayerLobby implements Screen {
+public class MultiplayerLobby extends GameState {
 
-    final Main game;
+
     OrthographicCamera camera;
     Vector2 touch;
+    SpriteBatch batch;
 
-    public MultiplayerLobby(Main game) {
-        this.game = game;
+    protected MultiplayerLobby(GameStateManager gsm) {
+        super(gsm);
+        init();
+    }
+
+
+    @Override
+    public void init() {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 640, 480);
+        batch = new SpriteBatch();
     }
 
     @Override
-    public void show() {
-
+    public void update(float dt) {
+        handleInput();
     }
 
     @Override
-    public void render(float delta) {
+    public void draw() {
         Gdx.gl.glClearColor(0.12f, 0.11f, 0.22f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         camera.update();
 
-        game.batch.setProjectionMatrix(camera.combined);
-        game.batch.begin();
-            Assets.font.draw(game.batch, "Multiplayer Lobby", 100, Main.GAME_HEIGHT - 100);
-            Assets.font.draw(game.batch, "Press ENTER to go to multiplayer Arena.", 100, Main.GAME_HEIGHT - 300);
-        game.batch.end();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        Assets.font.draw(batch, "Multiplayer Lobby", 100, Main.GAME_HEIGHT - 100);
+        Assets.font.draw(batch, "Press ENTER to go to multiplayer Arena.", 100, Main.GAME_HEIGHT - 300);
+        batch.end();
 
-        if (Gdx.input.isKeyPressed(Input.Keys.BACKSPACE)) {
-            game.setScreen(new ChooseSingleOrMulti(game));
+    }
+
+    @Override
+    public void handleInput() {
+        if (GameKeys.isPressed(GameKeys.BACKSPACE)) {
+            stateManager.setGameState(GameStateManager.CHOOSE_MODE);
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
-            game.setScreen(new MultiplayerArena(game));
+        if (GameKeys.isPressed(GameKeys.ENTER)) {
+            stateManager.setGameState(GameStateManager.PLAY_MULTIPLAYER);
         }
-    }
-
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
     }
 
     @Override

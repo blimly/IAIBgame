e212e15deffea22d0ff08ee739acc935b6f89868
Maario Lemmik <malemm@ttu.ee>
2020-04-05 11:08:31 +0300

    MainMenu -> Extends GameState new methods with same functionality, but different name

--- core/src/inc/heterological/iaibgame/desktop/screens/MainMenu.java
+++ core/src/inc/heterological/iaibgame/desktop/screens/MainMenu.java
@@ -1,98 +1,82 @@
 package inc.heterological.iaibgame.desktop.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import inc.heterological.iaibgame.desktop.Assets;
 import inc.heterological.iaibgame.desktop.Button;
 import inc.heterological.iaibgame.Main;
+import inc.heterological.iaibgame.desktop.managers.GameStateManager;
 
-public class MainMenu implements Screen {
+public class MainMenu extends GameState{
 
-    final Main game;
 
     OrthographicCamera camera;
     Vector2 touch;
     Button playButton;
     Button exitButton;
+    SpriteBatch batch;
 
     float bg_scroll_y;
 
-    public MainMenu(Main game) {
-        this.game = game;
+    public MainMenu(GameStateManager gsm) {
+        super(gsm);
+        init();
+    }
+
+
+    @Override
+    public void init() {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 640, 480);
         touch = new Vector2();
+        batch = new SpriteBatch();
         playButton = new Button(64 * 3, 24 * 3, 235, 120, Assets.play);
         exitButton = new Button(64 * 3, 24 * 3, 235, 30, Assets.exit);
         bg_scroll_y = 0;
     }
 
     @Override
-    public void show() {
-        Assets.menu_loop.loop(0.7f);
+    public void update(float dt) {
+
     }
 
     @Override
-    public void render(float delta) {
+    public void draw() {
         Gdx.gl.glClearColor(0.12f, 0.11f, 0.22f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         camera.update();
-        checkButtons(touch);
+        handleInput();
         bg_scroll_y -= 20 * Gdx.graphics.getDeltaTime();
 
-        game.batch.setProjectionMatrix(camera.combined);
-        game.batch.begin();
-            game.batch.draw(Assets.mainSpriteBack1, bg_scroll_y % 640, 0, 640, 480);
-            game.batch.draw(Assets.mainSpriteBack2, bg_scroll_y % 640 + 640, 0, 640, 480);
-            if (game.hasPlayedOnce) {
-                game.font.draw(game.batch, "press  [ h ]  for help", 245, 460);
-            }
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(Assets.mainSpriteBack1, bg_scroll_y % 640, 0, 640, 480);
+        batch.draw(Assets.mainSpriteBack2, bg_scroll_y % 640 + 640, 0, 640, 480);
 
-        playButton.drawButton(game.batch);
-            exitButton.drawButton(game.batch);
-        game.batch.end();
+        playButton.drawButton(batch);
+        exitButton.drawButton(batch);
+        batch.end();
     }
 
-    public void checkButtons(Vector2 touch) {
+    @Override
+    public void handleInput() {
         if(Gdx.input.isTouched()) {
             touch.set(Gdx.input.getX(), Main.GAME_HEIGHT - Gdx.input.getY());
             if(playButton.clicked(touch)) {
-                game.setScreen(new ChooseSingleOrMulti(game));
+                stateManager.setGameState(GameStateManager.PLAY);
             }
             if(exitButton.clicked(touch)) {
-                game.setScreen(new Bye(game));
+                Gdx.app.exit();
             }
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.H)) {
-            game.setScreen(new Hell(game));
-        }
-    }
-
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
 
     }
 
     @Override
     public void dispose() {
+        Assets.dispose();
     }
 }
